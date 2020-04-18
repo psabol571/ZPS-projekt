b1dc8570565f924dce55190461fd23a5183405d9
xewis <psabol571@gmail.com>
2020-04-18 21:50:06 +0200

    zatial final

---
+++ ZPS/nav.js
@@ -0,0 +1,9 @@
+//for collapsed Navigation
+function myFunction() {
+  var x = document.getElementById("hiddenNav");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
